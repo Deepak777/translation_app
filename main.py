e1f29dfd15e720b7a345c763ece9583aa9062d64
Deepak <tdeepakopy@gmail.com>
2025-07-09 16:24:52 +0530

    changed folder structure and changed short name

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def get_index():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="manifest" href="/static/manifest.json">
+        <link rel="manifest" href="manifest.json">
 
         <title>Audio Recorder with Speech Recognition</title>
         <style>
